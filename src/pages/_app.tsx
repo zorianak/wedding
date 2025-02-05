df8aef9dff7f378d99c92ebfa4ad787874aa8506
Callie Holmes <github@kimiscool.me>
2025-02-05 12:31:03 -0800

    rename page to home

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import Layout from '@/app/layout';
+import '@/app/globals.css';
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
+};
+
+export default MyApp;
